f04709870d3534088fd92a871285003389387d83
elyssiam0506 <elyssiam0506@gmail.com>
2025-09-02 21:07:59 -0500

    Updates

--- node_modules/kaplay/src/gfx/draw/drawSubstracted.ts
+++
@@ -1,8 +0,0 @@
-import { gfx } from "../../kaplay";
-import { drawStenciled } from "./drawStenciled";
-
-export function drawSubtracted(content: () => void, mask: () => void) {
-    const gl = gfx.ggl.gl;
-
-    drawStenciled(content, mask, gl.NOTEQUAL);
-}
